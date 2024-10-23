cfb21988c6f80091bb04765c994730c60fc1e663
Vinicius <viniciusmsantos02@gmail.com>
2024-10-23 16:17:25 -0300

    estruturação do banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+export default function Banner() {
+    return(
+        <>
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="Banner de boas-vindas da Organo" className="banner-img" />
+            </header>
+        </>
+    );
+}

73e28df8f1e78248fca81beca8fc436cbce3635a
Vinicius <viniciusmsantos02@gmail.com>
2024-10-24 21:02:43 -0300

    estruturação e componentização do select

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # Projeto de um Organograma feito a partir do React
 
-===
-
 ## Projeto desenvolvido a partir do curso **React: Desenvolvendo com JavaScript** - Alura
 
 ---
 
 ### O que aprendi:
 
-- A criar um novo projeto React do 0 npx `crate-react-app nome_do_projeto`;
+- A criar um novo projeto React do zero `npx crate-react-app nome_do_projeto`;
 - A criar componentes funcionais;
 - A estilizar componentes com CSS;
 - Analisei as dependências do projeto;
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,7 +1,19 @@
 import Input from '../Input'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 export default function Formulario() {
+
+    const times = [
+        'Vingadores',
+        'Guardiões da Galáxia',
+        'X-Men',
+        'Quarteto Fantástico',
+        'Super-Heroínas',
+        'Vilões',
+        'Eternos'
+    ]
+
     return (
         <>
             <section className="formulario">
@@ -11,6 +23,7 @@ export default function Formulario() {
                     <Input label="Nome" placeholder="Digite seu nome" />
                     <Input label="Poder" placeholder="Digite seu poder" />
                     <Input label="Imagem" placeholder="Digite o endereço de imagem" />
+                    <ListaSuspensa label="Times" itens={times} />
                 </form>
             </section>
         </>
